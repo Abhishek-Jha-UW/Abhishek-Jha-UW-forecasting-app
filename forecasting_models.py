c760bb8948b216b4e66094fecb2074f82e5564c1
Abhishek-Jha-UW <jhaabhishek0501@gmail.com>
2025-10-22 12:48:02 -0700

    Update forecasting_models.py

--- forecasting_models.py
+++ forecasting_models.py
@@ -17,7 +17,7 @@ class SimpleMA:
         sma_series = self.df[self.target_column].rolling(window=self.window).mean()
         last_sma = sma_series.dropna().iloc[-1]
         forecast_values = [last_sma] * self.steps
-        start_date = pd.Timestamp(self.df.index[-1]) + pd.Timedelta(weeks=1)
+        start_date = pd.Timestamp(self.df.index[-1]) + np.timedelta64(7, 'D')
         forecast_index = pd.date_range(start=start_date, periods=self.steps, freq='W')
         return pd.Series(forecast_values, index=forecast_index)
 
@@ -31,7 +31,7 @@ class ARIMAForecaster:
         model = ARIMA(self.series, order=self.order)
         fitted = model.fit()
         forecast = fitted.forecast(steps=self.steps)
-        start_date = pd.Timestamp(self.series.index[-1]) + pd.Timedelta(weeks=1)
+        start_date = pd.Timestamp(self.series.index[-1]) + np.timedelta64(7, 'D')
         forecast.index = pd.date_range(start=start_date, periods=self.steps, freq='W')
         return forecast
 
@@ -46,7 +46,7 @@ class SARIMAForecaster:
         model = SARIMAX(self.series, order=self.order, seasonal_order=self.seasonal_order)
         fitted = model.fit(disp=False)
         forecast = fitted.forecast(steps=self.steps)
-        start_date = pd.Timestamp(self.series.index[-1]) + pd.Timedelta(weeks=1)
+        start_date = pd.Timestamp(self.series.index[-1]) + np.timedelta64(7, 'D')
         forecast.index = pd.date_range(start=start_date, periods=self.steps, freq='W')
         return forecast
 
@@ -59,7 +59,7 @@ class ETSForecaster:
         model = ExponentialSmoothing(self.series, trend='add', seasonal=None)
         fitted = model.fit()
         forecast = fitted.forecast(self.steps)
-        start_date = pd.Timestamp(self.series.index[-1]) + pd.Timedelta(weeks=1)
+        start_date = pd.Timestamp(self.series.index[-1]) + np.timedelta64(7, 'D')
         forecast.index = pd.date_range(start=start_date, periods=self.steps, freq='W')
         return forecast
 
@@ -88,7 +88,7 @@ class XGBoostForecaster:
             preds.append(pred)
             last_row = np.roll(last_row, -1)
             last_row[0, -1] = pred
-        start_date = pd.Timestamp(self.df.index[-1]) + pd.Timedelta(weeks=1)
+        start_date = pd.Timestamp(self.df.index[-1]) + np.timedelta64(7, 'D')
         forecast_index = pd.date_range(start=start_date, periods=self.steps, freq='W')
         return pd.Series(preds, index=forecast_index)
 
